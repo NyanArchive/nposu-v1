d7794d8628f81cde26abb2f60de16c93735146a2
aeongdesu <osu@kakao.com>
2023-01-23 19:23:34 +0900

    chore: remove osuirc_cooldown
    because bancho.js already supported

--- example.js
+++ example.js
@@ -19,12 +19,6 @@ module.exports = {
     osuirc_username: "",
     osuirc_password: "",
 
-    /*
-        - Personal accounts can send 10 messages every 5 seconds
-        - Bot accounts can send 300 messages every 60 seconds
-    */
-    osuirc_cooldown: 5,
-
     // https://osu.ppy.sh/home/account/edit -> OAuth -> New OAuth Application
     osuv2_client_id: "",
     osuv2_client_secret: ""
